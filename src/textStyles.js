efd9cde5d8981577456e133d9754063262a9f734
ayush-raj8 <ayushraj763@gmail.com>
2024-05-24 02:22:01 +0530

    Custom Carousel

---
+++ src/textStyles.js
@@ -0,0 +1,25 @@
+const textStyles = {
+    header: {
+      fontFamily: 'Arial, sans-serif',
+      fontSize: '26px',
+      fontWeight: 'bold',
+    },
+    subheader: {
+      fontFamily: 'Arial, sans-serif',
+      fontSize: '20px',
+      fontWeight: 'normal',
+    },
+    body: {
+      fontFamily: 'Georgia, serif',
+      fontSize: '16px',
+      fontWeight: 'normal',
+    },
+    caption: {
+      fontFamily: 'Courier New, monospace',
+      fontSize: '12px',
+      fontWeight: 'lighter',
+    },
+  };
+  
+  export default textStyles;
+  
