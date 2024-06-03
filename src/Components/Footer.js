dc8f2cf9ef3fa9e0847c27cbe2dd801cbbd637cd
ayush-raj8 <ayushraj763@gmail.com>
2024-06-03 21:14:16 +0530

    Footer changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,27 +4,47 @@ import { FaYoutube, FaFacebook, FaInstagram, FaTwitter, FaLinkedin, FaEnvelope }
 import { useSelector } from 'react-redux';
 import { selectDarkModeStatus } from '../redux/selectors/darkModeSelector';
 import NrityaSVG from './NrityaSVG';
+import { styled } from '@mui/material/styles';
+
+
+
+
 
 function Footer() {
   const isDarkModeOn = useSelector(selectDarkModeStatus);
   const isLoggedIn = JSON.parse(localStorage.getItem('isLoggedIn'));
 
-  const socialIconStyle = { fontSize: '24px', color: 'white', marginRight: '10px' };
-  const linkStyle = { color: 'white', marginRight: '10px' };
+  const ResponsiveTypography = styled(Typography)(({ theme }) => ({
+    textTransform: 'none',
+    color: isDarkModeOn ? 'white':'black',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '1.5rem',
+    },
+  }));
+
+  const socialIconStyle = { fontSize: '24px', color: isDarkModeOn ? 'white' : 'black' , marginRight: '10px' };
+  const linkStyle = { color: isDarkModeOn ? 'white' : 'black' , marginRight: '10px' };
 
   return (
-    <div style={{ backgroundColor: isDarkModeOn ? 'black' : '#323232',}}>
+    <div style={{ backgroundColor: isDarkModeOn ? 'black' : '#D2D2D2'}}>
       <Divider />
-      <footer style={{ backgroundColor: isDarkModeOn ? 'black' : '#323232', padding: '15px 0 0' }}>
+      <footer style={{ backgroundColor: isDarkModeOn ? 'black' : '#D2D2D2', padding: '15px 0 0' }}>
         <Container>
           <Grid container justifyContent="space-between" alignItems="center">
-            <Grid item xs={12} sm={6} md={3} textAlign="center" py={1}>
-              <Typography variant="body2" style={{ color:'white'}}>
-                India's first dedicated dance-tech connecting dance enthusiasts to dance studios. Learner's get a free trial from studio. Discover the beat in your city.
+          <Grid container justifyContent="center" py={1} >
+          <ResponsiveTypography variant="h4">
+            Discover the beat in your city!
+          </ResponsiveTypography>
+
+ 
+          </Grid>
+            <Grid item xs={12} sm={12} md={12} textAlign="center" py={1}>
+              <Typography variant="body1" style={{ textAlign: 'left',color:isDarkModeOn ? 'white':'black'}}>
+                Introducing India's first dance tech platform connecting dance enthusiasts with top dance studios in the city. Enjoy a free trial class at your chose dance studio and immerse yourself in the world of dance. Start your journey with us today! 
               </Typography>
             </Grid>
             <Grid item xs={12} sm={6} md={3} textAlign="center" py={1}>
-              <Typography variant="h6" style={{ color: 'white' ,fontSize: 20 }}>
+              <Typography variant="h6" style={{ color:isDarkModeOn ? 'white':'black',fontSize: 20 }}>
                 Follow Us
               </Typography>
               <div>
@@ -40,7 +60,7 @@ function Footer() {
               </div>
             </Grid>
             <Grid item xs={12} sm={6} md={3} textAlign="center" py={1}>
-              <Typography variant="h6" style={{ color:  'white' ,fontSize: 20 }}>
+              <Typography variant="h6" style={{color:isDarkModeOn ? 'white':'black',fontSize: 20 }}>
                 About
               </Typography>
               <Typography variant="body2">
@@ -49,16 +69,11 @@ function Footer() {
                <Typography variant="body2">
                 <a href="#/contactus" style={linkStyle}>Contact Us</a>
               </Typography>
-              <Typography variant="body2">
-                <a href="#/tnc" style={linkStyle}>T&C</a>
-              </Typography>
+            
             </Grid>
             <Grid item xs={12} sm={6} md={3} textAlign="center" py={1}>
-              <Typography variant="h6" style={{ color:  'white',fontSize: 20}}>
+              <Typography variant="h6" style={{ color: isDarkModeOn ? 'white' : 'black' ,fontSize: 20}}>
                 Quick Links
-              </Typography>
-              <Typography variant="body2">
-                <a href="#/jobs" style={linkStyle}>Job Hire</a>
               </Typography>
               <Typography variant="body2">
                 <a href="#/modifyStudios" style={linkStyle}>List Studios</a>
@@ -71,19 +86,17 @@ function Footer() {
               </Typography>
             </Grid>
           </Grid>
-          <Grid container justifyContent="center" py={1} >
-              <NrityaSVG text={"Discover the beat in your city!"} x={"0"} y={"10"} fontSize={"5px"}/>
-          </Grid>
+          
         </Container>
 
         <Grid justifyContent="center" py={1}
           style={{
-            backgroundColor: isDarkModeOn ? 'black' : '#323232',
+            backgroundColor: isDarkModeOn ? 'black' : '#D2D2D2',
             padding: '15px 0 8px',
             textAlign: 'center'
           }}
         >
-            <Typography variant="body2" style={{fontSize: '12px', color: isDarkModeOn ? 'white' : 'white' }}>
+            <Typography variant="body2" style={{fontSize: '12px', color: isDarkModeOn ? 'white' : 'black' }}>
               Copyright &copy; 2024 Nritya. All Rights Reserved.</Typography>
           </Grid>
       </footer>
