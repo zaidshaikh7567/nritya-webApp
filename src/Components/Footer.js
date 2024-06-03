2c6134bd076f749ef1e8faa82a2d13c62f18c3f4
ayush-raj8 <ayushraj763@gmail.com>
2024-06-03 20:36:27 +0530

    Fixing footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,22 +9,22 @@ function Footer() {
   const isDarkModeOn = useSelector(selectDarkModeStatus);
   const isLoggedIn = JSON.parse(localStorage.getItem('isLoggedIn'));
 
-  const socialIconStyle = { fontSize: '24px', color: isDarkModeOn ? 'white' : 'black', marginRight: '10px' };
-  const linkStyle = { color: isDarkModeOn ? 'white' : 'black', marginRight: '10px' };
+  const socialIconStyle = { fontSize: '24px', color: 'white', marginRight: '10px' };
+  const linkStyle = { color: 'white', marginRight: '10px' };
 
   return (
-    <div style={{ backgroundColor: isDarkModeOn ? 'black' : 'white' }}>
+    <div style={{ backgroundColor: isDarkModeOn ? 'black' : '#323232',}}>
       <Divider />
-      <footer style={{ background: isDarkModeOn ? 'black' : 'white', padding: '15px 0 0' }}>
+      <footer style={{ backgroundColor: isDarkModeOn ? 'black' : '#323232', padding: '15px 0 0' }}>
         <Container>
           <Grid container justifyContent="space-between" alignItems="center">
             <Grid item xs={12} sm={6} md={3} textAlign="center" py={1}>
-              <Typography variant="body2" style={{ color: isDarkModeOn ? 'white' : 'black' }}>
+              <Typography variant="body2" style={{ color:'white'}}>
                 India's first dedicated dance-tech connecting dance enthusiasts to dance studios. Learner's get a free trial from studio. Discover the beat in your city.
               </Typography>
             </Grid>
             <Grid item xs={12} sm={6} md={3} textAlign="center" py={1}>
-              <Typography variant="h6" style={{ color: isDarkModeOn ? 'white' : 'black',fontSize: 20 }}>
+              <Typography variant="h6" style={{ color: 'white' ,fontSize: 20 }}>
                 Follow Us
               </Typography>
               <div>
@@ -40,7 +40,7 @@ function Footer() {
               </div>
             </Grid>
             <Grid item xs={12} sm={6} md={3} textAlign="center" py={1}>
-              <Typography variant="h6" style={{ color: isDarkModeOn ? 'white' : 'black',fontSize: 20 }}>
+              <Typography variant="h6" style={{ color:  'white' ,fontSize: 20 }}>
                 About
               </Typography>
               <Typography variant="body2">
@@ -54,7 +54,7 @@ function Footer() {
               </Typography>
             </Grid>
             <Grid item xs={12} sm={6} md={3} textAlign="center" py={1}>
-              <Typography variant="h6" style={{ color: isDarkModeOn ? 'white' : 'black' ,fontSize: 20}}>
+              <Typography variant="h6" style={{ color:  'white',fontSize: 20}}>
                 Quick Links
               </Typography>
               <Typography variant="body2">
@@ -78,7 +78,7 @@ function Footer() {
 
         <Grid justifyContent="center" py={1}
           style={{
-            backgroundColor: isDarkModeOn ? '#3a3a3a' : '#323232',
+            backgroundColor: isDarkModeOn ? 'black' : '#323232',
             padding: '15px 0 8px',
             textAlign: 'center'
           }}
